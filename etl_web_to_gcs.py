11a9a071a240c15d1b135a0b6e87d1d363cc447a
Mahdi-Moosa <82473321+Mahdi-Moosa@users.noreply.github.com>
2023-02-03 18:58:18 -0500

    Initial UL, Prefect via GitHub

---
+++ etl_web_to_gcs.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from random import randint
+
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas DataFrame"""
+    # if randint(0, 1) > 0:
+    #     raise Exception
+
+    df = pd.read_csv(dataset_url)
+    return df
+
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    # print(df.columns)
+    if "tpep_pickup_datetime" in df.columns:
+        df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+        df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    elif "lpep_pickup_datetime" in df.columns:
+        df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+        df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
+@task()
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet file"""
+    import os
+    folder_path = f"data/{color}"
+    if not os.path.exists(folder_path):
+        print(f"Destination path : {folder_path} does not exist. Creating folder.")
+        os.makedirs(folder_path)
+        print(f'Folder (with sub-folder) created: {folder_path}')
+
+    path = Path(f"{folder_path}/{dataset_file}.parquet")
+    df.to_parquet(path, compression="gzip")
+    return path
+
+
+@task()
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("de-zoomcamp-gcs")
+    gcs_block.upload_from_path(from_path=path, to_path=path)
+    return
+
+
+@flow()
+def etl_web_to_gcs(color : str, year: int, month: int) -> None:
+    """The main ETL function"""
+    # color = "green"
+    # year = 2020
+    # month = 1
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path)
+
+@flow()
+def etl_parent_flow(months: list[int], year: int, color: str):
+    for month in months:
+        etl_web_to_gcs(color, year, month)
+
+if __name__ == "__main__":
+    color = 'yellow'
+    year = 2019
+    months = [2,3]
+    etl_parent_flow(color=color, months = months, year = year)
